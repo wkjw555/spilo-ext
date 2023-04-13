86e181e2a873c4d3668ef114aa223371509811d9
wkjw555 <105335296+wkjw555@users.noreply.github.com>
2023-04-13 15:41:27 +0800

    Update dependencies.sh

--- postgres-appliance/build_scripts/dependencies.sh
+++ postgres-appliance/build_scripts/dependencies.sh
@@ -20,30 +20,11 @@ ARCH="$(dpkg --print-architecture)"
 
 echo -e 'APT::Install-Recommends "0";\nAPT::Install-Suggests "0";' > /etc/apt/apt.conf.d/01norecommend
 
-apt-get update
-apt-get install -y curl ca-certificates
-
-apt-get install -y software-properties-common gpg-agent
-add-apt-repository ppa:longsleep/golang-backports
-apt-get update
-apt-get install -y golang-go liblzo2-dev brotli libsodium-dev git make cmake gcc libc-dev
 go version
+#go env -w GO111MODULE=on
+#go env -w GOPROXY=https://goproxy.cn,direct
 
 git clone -b "$WALG_VERSION" --recurse-submodules https://github.com/wal-g/wal-g.git
 cd /wal-g
 go get -v -t -d ./...
 go mod vendor
-
-bash link_brotli.sh
-bash link_libsodium.sh
-
-export USE_LIBSODIUM=1
-export USE_LZO=1
-make pg_build
-
-# We want to remove all libgdal30 debs except one that is for current architecture.
-printf "shopt -s extglob\nrm /builddeps/!(*_%s.deb)" "$ARCH" | bash -s
-
-mkdir /builddeps/wal-g
-
-cp /wal-g/main/pg/wal-g /builddeps/wal-g/
